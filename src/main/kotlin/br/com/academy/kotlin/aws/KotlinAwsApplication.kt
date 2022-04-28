25139642f9f4396ad4b7de536a7036f3b822238e
Nesomar Ramalho Freire <nfreire@latam.stefanini.com>
2022-04-28 11:47:29 -0300

    estrutura inicial

---
+++ src/main/kotlin/br/com/academy/kotlin/aws/KotlinAwsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.academy.kotlin.aws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAwsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAwsApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/academy/kotlin/aws/KotlinAwsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.academy.kotlin.aws
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinAwsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
